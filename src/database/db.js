8a73ada0832616d69ed1908f95bbff4469cd511f
Victor Parizio <victorparizio@hotmail.com>
2023-05-20 12:29:42 -0300

    feat (index, db) Conectando ao MongoDB Atlas Foi feita a instalação do mongoose para conectar o backend ao MongoDB Atlas. O arquivo db.js está responsável por fazer a conecção.

---
+++ src/database/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const connectDatabase = () => {
+    console.log('Wait connecting to the database')
+
+    mongoose
+        .connect(
+            mongoose.connect('mongodb+srv://vmpariziog:DeVip@cluster0.l5ibydd.mongodb.net/?retryWrites=true&w=majority'),
+            { useNewUrlParser: true, useUnifiedTopology: true }
+        )
+        .then(()=> console.log('MongoDB Atlas Connected'))
+        .catch((error) => console.log(error))
+}
+
+module.exports = connectDatabase
