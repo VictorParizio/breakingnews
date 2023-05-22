4354028e1bc089241ced64a87f531eca762088ee
Victor Parizio <victorparizio@hotmail.com>
2023-05-22 09:50:55 -0300

    feat (user.controller, user.route, user.service) Atualização de dados do usuário Foi adicionada uma rota de tipo patch para atualizar ao menos um campo do usuário de acordo com o ip fornecido.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -40,7 +40,7 @@ const findAllUsers = async (req, res) => {
 const findById = async (req, res) => {
     const id = req.params.id
 
-    if(!mongoose.Types.ObjectId.isValid(id)){
+    if (!mongoose.Types.ObjectId.isValid(id)) {
         return res.status(400).send({ message: "Invalid ID" })
     }
 
@@ -53,8 +53,41 @@ const findById = async (req, res) => {
     res.send(user)
 }
 
+const update = async (req, res) => {
+    const { name, username, email, password, avatar, background } = req.body
+
+    if (!name && !username && !email && !password && !avatar && !background) {
+        return res.status(400).send({ message: "Submit at least one field for update" })
+    }
+
+    const id = req.params.id
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).send({ message: "Invalid ID" })
+    }
+
+    const user = await userService.findByIdService(id)
+
+    if (!user) {
+        return res.status(400).send({ message: "User not found" })
+    }
+
+    await userService.updateService(
+        id,
+        name,
+        username,
+        email,
+        password,
+        avatar,
+        background
+    )
+
+    res.send({ message: "User successfully update!" })
+}
+
 module.exports = {
     create,
     findAllUsers,
     findById,
+    update
 }
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -4,5 +4,6 @@ const userController = require('../controllers/user.controller')
 route.post("/", userController.create)
 route.get("/", userController.findAllUsers)
 route.get("/:id", userController.findById)
+route.patch("/:id", userController.update)
 
 module.exports = route
