23a5d7413551c957f0d0fc46e639f96bbb197642
Victor Parizio <victorparizio@hotmail.com>
2023-05-29 09:03:09 -0300

    feat (auth.route, auth.controller, auth.service) Login e Validação de Usuário Foram adicionadas rota, controller e service para verificar a validade dos dados e fazer o login a partir dessas informações.

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 import express from 'express'
-import connectDatabase from './src/database/database.js'
-import userRoute from './src/routes/user.route.js'
 import dotenv from 'dotenv'
 
-dotenv.config()
+import connectDatabase from './src/database/database.js'
+
+import userRoute from './src/routes/user.route.js'
+import authRoute from './src/routes/auth.route.js'
+
 const app = express()
 const port = process.env.PORT || 3000
 
+dotenv.config()
 connectDatabase()
+
 app.use(express.json())
 app.use("/user", userRoute)
+app.use("/auth", authRoute)
 
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`))
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,8 +1,8 @@
-import express from 'express' 
-import userController from'../controllers/user.controller.js'
-import {validId, validUser} from'../middleware/global.middlewares.js'
+import { Router } from 'express'
+import userController from '../controllers/user.controller.js'
+import { validId, validUser } from '../middleware/global.middlewares.js'
 
-const route = express.Router()
+const route = Router()
 
 route.post("/", userController.create)
 route.get("/", userController.findAllUsers)
